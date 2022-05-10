b1c4d54247f04f9f7f922c7ef80f8d1902193216
wh0101 <weihongliuu@gmail.com>
2022-05-11 03:09:57 +0800

    retry

--- main.js
+++ main.js
@@ -45,25 +45,40 @@ class Field{
         }
         
         //set the "hat" location 
+        
         let y = Math.floor(Math.random() * row)
         let x =Math.floor(Math.random() * col)
         this.field[y][x]= hat;
+        
+        while(hat.y===0 && hat.x===0){
+            let y = Math.floor(Math.random() * row)
+            let x =Math.floor(Math.random() * col)
+            this.field[y][x]= hat;
+        }
+        
 
         //set Character position as [0][0]
         this.field[this.locationY][this.locationX] = pathCharacter;
         
         //set holes
-        
-        for (let a=(Math.floor(Math.random()*col )); a<row; a++){
-            for (let b =(Math.floor(Math.random()*row)); b<col; b++){
+        for (let a= (Math.floor(Math.random()*row)); a<row; a++){
+            for (let b = (Math.floor(Math.random()*col)); b<col; b++){
                 this.field[a][b] = hole;
             }
+        }
+
+        if ((hole.a===0 && hole.b===0) || (hole.a===hat.y && hole.b===hat.x)){
+            let a = Math.floor(Math.random() * row)
+            let b =Math.floor(Math.random() * col)
+            this.field[a][b] = hole;
+
+        }
+
         
     }
-    
-    }
+
     runGame() {
-        //Implement your codes
+       // while
         this.print();
         this.askQuestions();
         
@@ -78,22 +93,22 @@ class Field{
     askQuestions(){
         const answer = prompt('which way? ').toUpperCase();
         
-        if (this.answer ="u"){
+        if (answer ==="u"){
             console.log("Moving Up")
             this.locationY -= 1
             
         }
-        else if (this.answer="d"){
+        else if (answer==="d"){
             console.log("Moving Down")
             this.locationY += 1
             
         }
-        else if(this.answer="l"){
+        else if(answer==="l"){
             console.log("moving Left")
             this.locationX -= 1
             
         }
-        else if(this.answer="r"){
+        else if(answer==="r"){
             console.log("Moving Right")
             this.locationX += 1
             
@@ -103,7 +118,11 @@ class Field{
             console.log("Please key in a U D L R for moving up, downn, left, or right only")
             
         }
-    }
+        
+    
+    }//end of questions 
+
+
 
 
 
